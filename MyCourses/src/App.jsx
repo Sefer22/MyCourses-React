46cb2f7e814a6b41acd6b5113242696c96d28acb
Sefer22 <120428751+Sefer22@users.noreply.github.com>
2024-06-17 02:04:06 +0400

    developed

--- MyCourses/src/App.jsx
+++ MyCourses/src/App.jsx
@@ -1,12 +1,13 @@
 import { useState } from 'react'
 import reactLogo from './assets/react.svg'
 import './App.css'
+import Header from './Header'
 
 function App() {
 
   return (
     <div>
-      Safar
+      < Header />
     </div>
   )
 }
