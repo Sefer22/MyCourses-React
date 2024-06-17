584ebf827b2434c60366765a35fa7c5233b9d061
Sefer22 <120428751+Sefer22@users.noreply.github.com>
2024-06-17 20:25:16 +0400

    developed

--- MyCourses/src/App.jsx
+++ MyCourses/src/App.jsx
@@ -3,17 +3,20 @@ import './App.css'
 import Header from './Header'
 import { courses } from './Data'
 import Course from './Course'
+import './css/Course.css';
 
 function App() {
 
   return (
     <div>
       <Header />
-      {
-        courses?.map((course) => (
-          <Course key={course.id} course={course} />
-        ))
-      }
+      <div className='course-main'>
+        {
+          courses?.map((course) => (
+            <Course key={course.id} course={course} />
+          ))
+        }
+      </div>
     </div>
   )
 }
--- MyCourses/src/Course.jsx
+++ MyCourses/src/Course.jsx
@@ -4,10 +4,10 @@ function Course({ course }) {
     const { id, title, description, price, link, image } = course;
     return (
         <div className='course'>
-            <div>
+            <div className='course'>
                 <img src={image} width={250} height={150} />
-                <h4>{title}</h4>
-                <h5>{description}</h5>
+                <h4 className='course-title'>{title}</h4>
+                <p className='course-desc'>{description}</p>
                 <h3>{price}</h3>
                 <a href={link}>For buying</a>
             </div>
