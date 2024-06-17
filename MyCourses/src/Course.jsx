f218c037611b9bcf39a021ce2d21867d3288687e
Sefer22 <120428751+Sefer22@users.noreply.github.com>
2024-06-17 19:55:28 +0400

    nn

---
+++ MyCourses/src/Course.jsx
@@ -0,0 +1,10 @@
+import React from 'react'
+
+function Course({ course }) {
+    const { id, title, description, price, link, image } = course;
+    return (
+        <div>Course</div>
+    )
+}
+
+export default Course
