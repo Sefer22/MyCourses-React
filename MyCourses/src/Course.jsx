677f155670a75352f92508c14fa4b2d73910f416
Sefer22 <120428751+Sefer22@users.noreply.github.com>
2024-06-17 19:59:42 +0400

    developed

--- MyCourses/src/Course.jsx
+++ MyCourses/src/Course.jsx
@@ -3,7 +3,16 @@ import React from 'react'
 function Course({ course }) {
     const { id, title, description, price, link, image } = course;
     return (
-        <div>Course</div>
+        <div className='course'>
+            <div>
+                <img src={image} width={250} height={200} />
+                <h4>{title}</h4>
+                <h5>{description}</h5>
+                <h3>{price}</h3>
+                <h6>{link}</h6>
+            </div>
+
+        </div>
     )
 }
 
