49f8cec3dc2bdd4af6d543c987d239b3924316eb
Sefer22 <120428751+Sefer22@users.noreply.github.com>
2024-06-17 19:15:14 +0400

    nn

--- MyCourses/src/App.jsx
+++ MyCourses/src/App.jsx
@@ -1,7 +1,7 @@
 import { useState } from 'react'
-import reactLogo from './assets/react.svg'
 import './App.css'
 import Header from './Header'
+import { courses } from './Data'
 
 function App() {
 
--- MyCourses/src/Data.jsx
+++ MyCourses/src/Data.jsx
@@ -1,6 +1,44 @@
-const courses = [
+import Java from './images/1.jpg'
+import Js from './images/2.jpg'
+import C from './images/3.png'
+import CyberSecurity from './images/4.jpg'
+
+
+export const courses = [
     {
-        "id": 1244,
-        "title": "Java"
+        id: 1244,
+        title: "Java Programming Language",
+        description: "Lorem, ipsum dolor sit amet consectetur adipisicing elit. Maxime doloremque quo dolore, eius nesciunt placeat soluta odit et ipsa aspernatur.",
+        price: 120,
+        link: "https://www.javaguides.net/",
+        image: Java
+
+    },
+    {
+        id: 2345,
+        title: "JavaScript Programming Language",
+        description: "Lorem, ipsum dolor sit amet consectetur adipisicing elit. Maxime doloremque quo dolore, eius nesciunt placeat soluta odit et ipsa aspernatur.",
+        price: 220,
+        link: "https://javascript.info/",
+        image: Js
+
+    },
+    {
+        id: 5674,
+        title: "C Programming Language",
+        description: "Lorem, ipsum dolor sit amet consectetur adipisicing elit. Maxime doloremque quo dolore, eius nesciunt placeat soluta odit et ipsa aspernatur.",
+        price: 320,
+        link: "https://www.learn-c.org/",
+        image: C
+
+    },
+    {
+        id: 4765,
+        title: "Cyber Security Course",
+        description: "Lorem, ipsum dolor sit amet consectetur adipisicing elit. Maxime doloremque quo dolore, eius nesciunt placeat soluta odit et ipsa aspernatur.",
+        price: 300,
+        link: "https://www.cybrary.it/",
+        image: CyberSecurity
+
     }
 ]
