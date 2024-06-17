9f2ee11735fb2ff184a4efe3fee36643f1f14404
Sefer22 <120428751+Sefer22@users.noreply.github.com>
2024-06-17 20:26:52 +0400

    developed

--- MyCourses/src/Course.jsx
+++ MyCourses/src/Course.jsx
@@ -8,7 +8,7 @@ function Course({ course }) {
                 <img src={image} width={250} height={150} />
                 <h4 className='course-title'>{title}</h4>
                 <p className='course-desc'>{description}</p>
-                <h3>{price}</h3>
+                <h3 className='course-price'>{price}</h3>
                 <a href={link}>For buying</a>
             </div>
 
